eab1850fd0f25c12bdb0a58ff318f6b974973a2a
u1in <u1in@qq.com>
2026-03-24 09:21:17 +0800

    fix(draw): 修复渲染时背景填充逻辑以确保正确显示

--- src/editor/core/draw/particle/latex/LaTexParticle.ts
+++ src/editor/core/draw/particle/latex/LaTexParticle.ts
@@ -21,17 +21,24 @@ export class LaTexParticle extends ImageParticle {
     const { scale } = this.options
     const width = element.width! * scale
     const height = element.height! * scale
+    const originFillStyle = ctx.fillStyle
     if (this.imageCache.has(element.value)) {
       const img = this.imageCache.get(element.value)!
       ctx.clearRect(x, y, width, height)
+      ctx.fillStyle = 'white'
+      ctx.fillRect(x - 1, y - 1, width + 2, height + 2)
       ctx.drawImage(img, x, y, width, height)
+      ctx.fillStyle = originFillStyle
     } else {
       const laTexLoadPromise = new Promise((resolve, reject) => {
         const img = new Image()
         img.src = element.laTexSVG!
         img.onload = () => {
           ctx.clearRect(x, y, width, height)
+          ctx.fillStyle = 'white'
+          ctx.fillRect(x - 1, y - 1, width + 2, height + 2)
           ctx.drawImage(img, x, y, width, height)
+          ctx.fillStyle = originFillStyle
           this.imageCache.set(element.value, img)
           resolve(element)
         }
