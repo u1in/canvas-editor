058f482e36e424f91cae20c6cfc6ae9a29b2f013
u1in <u1in@qq.com>
2026-04-15 09:01:30 +0800

    fix(latex): 修复LaTex粒子异步渲染导致的显示问题

--- src/editor/core/draw/particle/latex/LaTexParticle.ts
+++ src/editor/core/draw/particle/latex/LaTexParticle.ts
@@ -21,26 +21,20 @@ export class LaTexParticle extends ImageParticle {
     const { scale } = this.options
     const width = element.width! * scale
     const height = element.height! * scale
-    const originFillStyle = ctx.fillStyle
     if (this.imageCache.has(element.value)) {
       const img = this.imageCache.get(element.value)!
-      ctx.clearRect(x, y, width, height)
-      ctx.fillStyle = 'white'
-      ctx.fillRect(x - 1, y - 1, width + 2, height + 2)
       ctx.drawImage(img, x, y, width, height)
-      ctx.fillStyle = originFillStyle
     } else {
+      const cacheRenderCount = this.getRenderCount()
       const laTexLoadPromise = new Promise((resolve, reject) => {
         const img = new Image()
         img.src = element.laTexSVG!
         img.onload = () => {
-          ctx.clearRect(x, y, width, height)
-          ctx.fillStyle = 'white'
-          ctx.fillRect(x - 1, y - 1, width + 2, height + 2)
-          ctx.drawImage(img, x, y, width, height)
-          ctx.fillStyle = originFillStyle
           this.imageCache.set(element.value, img)
           resolve(element)
+          // 因图片加载异步，图片加载后可能属于上一次渲染方法
+          if (cacheRenderCount !== this.getRenderCount()) return
+          ctx.drawImage(img, x, y, width, height)
         }
         img.onerror = error => {
           reject(error)
